896493fa489ae862cb3362515c4a78863f593f7c
akash alluarjun <akashs271997@gmail.com>
2020-07-06 18:28:05 +0530

    Akash

---
+++ AreaofRect.js
@@ -0,0 +1,15 @@
+const readline = require('readline');
+const inp = readline.createInterface({
+  input: process.stdin
+});
+const userInput = [];
+inp.on("line", (data) => {
+ userInput.push(data);
+});
+inp.on("close", () => {
+
+let a = userInput[0];
+let b = userInput[1];
+let c = a*b;
+console.log(c);
+});
